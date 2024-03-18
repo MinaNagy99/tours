840a02ff360ff779948a7d7ce338067e97f01eee
MinaNagy99 <minanagykhalefa@gmail.com>
2024-03-18 10:12:46 +0200

    export webhook

--- src/payment/payment.router.js
+++ src/payment/payment.router.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { auth } from "../../middlewares/auth.js";
-import { handleSuccessPayment, sessionCheckout } from "./payment.controller.js";
+import { handleSuccessPayment, sessionCheckout, webhook } from "./payment.controller.js";
 const paymentRouter = express.Router();
 
 paymentRouter.post("/checkout-session", auth, sessionCheckout);
 paymentRouter.get('/success', handleSuccessPayment);
-paymentRouter.post('/webhook', );
+paymentRouter.post('/webhook', webhook);
 export default paymentRouter;
