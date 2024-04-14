4853fe7767bc86dcaed15258eb3cb48d0b99b78c
MinaNagy99 <you@example.com>
2024-04-14 11:16:19 +0200

    s'

--- src/payment/payment.router.js
+++ src/payment/payment.router.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { auth } from "../../middlewares/auth.js";
-import { handleSuccessPayment, sessionCheckout, webhook } from "./payment.controller.js";
+import {
+  handleSuccessPayment,
+  sessionCheckout,
+  webhook,
+} from "./payment.controller.js";
 const paymentRouter = express.Router();
 
 paymentRouter.post("/checkout-session/:id", auth, sessionCheckout);
-paymentRouter.get('/success', handleSuccessPayment);
-paymentRouter.post('/handelPassCheckout', webhook,handleSuccessPayment);
+paymentRouter.get("/success", webhook, handleSuccessPayment);
 export default paymentRouter;
